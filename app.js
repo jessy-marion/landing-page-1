f8c046bc28b93aebbd1977fc22e149f3a8f9cb32
Jessy <jessymarionmail@gmail.com>
2023-11-27 17:15:17 +0100

    js: fix hamburger menu && navbar

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const header = document.querySelector('header');
 const menu = document.querySelector('.menu');
 const menuItems = document.querySelectorAll('.menuItem');
 const menuIcon = document.querySelector('.menuIcon');
@@ -5,12 +6,14 @@ const closeIcon = document.querySelector('.closeIcon');
  
 
 function toggleMenu() {
-    if (menu.classList.contains('showMenu')) {
+    if (menu.classList.contains('showMenu') && window.innerWidth <= 768 ) {
         menu.classList.remove('showMenu');
+        menu.classList.add('invisible');
         closeIcon.style.display = "none";
         menuIcon.style.display = "block";
-    } else {
+    } else if (window.innerWidth <= 768) {
         menu.classList.add('showMenu');
+        menu.classList.remove('invisible');
         closeIcon.style.display = "block";
         menuIcon.style.display = "none";
     }
@@ -28,24 +31,36 @@ menuItems.forEach( function(menuItem) {
 
 // Fonction pour afficher le menu hamburger sur les versions mobiles et tablettes
 function showHamburgerMenu() {
-    // Code pour afficher le menu hamburger
-    
+   menuIcon.style.display = "block";
+   menu.classList.remove("navBar");
+    menu.classList.add("invisible");
+   menu.classList.add("menu");
+   
 }
 
 // Fonction pour afficher la navbar sur les versions desktop
 function showNavbar() {
-    // Code pour afficher la navbar
+     menuIcon.style.display = "none";
+     menu.classList.remove("invisible");
+     menu.classList.remove("menu");
+     menu.classList.add("navBar");
+
+    // header.style.removeProperty('position');
+
+   
 }
 
 // Vérifier la largeur de l'écran pour décider d'afficher le menu hamburger ou la navbar
 function checkScreenWidth() {
     if (window.innerWidth <= 768) {
         showHamburgerMenu();
+        console.log("hamburger");
     } else {
         showNavbar();
+        console.log("navbar");
     }
 }
 
-// Appeler la fonction checkScreenWidth au chargement de la page et lors du redimensionnement de la fenêtre
+// 
 window.addEventListener('load', checkScreenWidth);
 window.addEventListener('resize', checkScreenWidth);
